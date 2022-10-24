06abc91195d5ec5a866f726aa51d2f7f674dacef
njoro410 <brian.njoroge@student.moringaschool.com>
2022-10-24 14:37:47 +0300

    added logic to display Todo

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {FaRegTrashAlt} from "react-icons/fa"
 
-const Todo = () => {
+const styles = {
+    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+    row: `flex gap-2`,
+    text: `cursor-pointer`,
+    textComplete: `cursor-pointer line-through`,
+    button: `cursor-pointer flex items-center`
+}
+
+const Todo = ({todo, toggleComplete, deleteTodo}) => {
   return (
-    <li>Todo</li>
+    <li className={todo.completed ? styles.liComplete : styles.li}>
+        <div className={styles.row}>
+            <input onChange={()=>toggleComplete(todo)} type="checkbox" checked={todo.completed}/>
+            <p onClick={()=>toggleComplete(todo)} className={todo.completed ? styles.textComplete : styles.text}>{todo.text}</p>
+        </div>
+        <button onClick={()=>deleteTodo(todo.id)}>{<FaRegTrashAlt/>}</button>
+    </li>
   )
 }
 
