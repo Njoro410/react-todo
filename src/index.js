2e32564a6d23ab73fb536a984fae5490440ab11b
njoro410 <brian.njoroge@student.moringaschool.com>
2022-10-24 15:42:11 +0300

    added a service worker for creating a pwa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';                                      
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-
+serviceWorker.register();
