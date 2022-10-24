c9363ab5c2cf7b149c097848101277ecb737c1f6
njoro410 <brian.njoroge@student.moringaschool.com>
2022-10-24 14:58:24 +0300

    configured .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -6,12 +7,19 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAr1VUtP_-sKTOxZQpuDKIRG4QUWj35Ga0",
-  authDomain: "react-todo-36549.firebaseapp.com",
-  projectId: "react-todo-36549",
-  storageBucket: "react-todo-36549.appspot.com",
-  messagingSenderId: "507909574853",
-  appId: "1:507909574853:web:49e1c3047c0b994ea8a0ac"
+  // apiKey: "AIzaSyAr1VUtP_-sKTOxZQpuDKIRG4QUWj35Ga0",
+  // authDomain: "react-todo-36549.firebaseapp.com",
+  // projectId: "react-todo-36549",
+  // storageBucket: "react-todo-36549.appspot.com",
+  // messagingSenderId: "507909574853",
+  // appId: "1:507909574853:web:49e1c3047c0b994ea8a0ac"
+
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
